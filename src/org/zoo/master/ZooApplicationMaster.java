3d5dea1e6fc24fca4f6258c654f68a9a2719aed8
Prakash Chandra Chhipa <prakash.chandra.chhipa@gmail.com>
2018-05-06 00:05:49 +0530

    Updated code with bug fixes

--- src/org/zoo/master/ZooApplicationMaster.java
+++ src/org/zoo/master/ZooApplicationMaster.java
@@ -39,7 +39,7 @@ public class ZooApplicationMaster {
 		for (int requestCount = 0; requestCount < 10; requestCount++) {
 			/*** USE CASE 1 : creating and Adding Wild Animal ***/
 			/* Wild Animal getting created in below statement */
-			Animal wildAnimal = zooManager.createWildAnimal("Wild", 10, 10);
+			Animal wildAnimal = zooManager.createWildAnimal("Wild", 10, requestCount);
 			/*
 			 * Keeping animal in suitable cage which follow the required
 			 * business rule Note : In case, all cages are occupied then it iwll
